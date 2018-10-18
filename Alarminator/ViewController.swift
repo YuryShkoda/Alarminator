7bf20734cbeb2d4e05f8f5fb59d604852c04c299
Yury Shkoda <shkoda.yuri@gmail.com>
2018-10-18 19:02:10 -0400

    Initial commit

---
+++ Alarminator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alarminator
+//
+//  Created by Yury on 10/18/18.
+//  Copyright © 2018 Yury Shkoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
