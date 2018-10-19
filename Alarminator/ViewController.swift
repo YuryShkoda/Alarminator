c1607a68fd80b0501021551eb05292385fc5e7a6
Yury Shkoda <shkoda.yuri@gmail.com>
2018-10-19 14:07:09 -0400

    Added groups listing

--- Alarminator/ViewController.swift
+++ Alarminator/ViewController.swift
@@ -13,6 +13,47 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let titleAttributes = [NSAttributedString.Key.font: UIFont(name: "Arial Rounded MT Bold", size: 20)!]
+        navigationController?.navigationBar.titleTextAttributes = titleAttributes
+        title = "Alarminator"
+        
+        groups.append(Group(name: "Enabled group", playSound: true, enabled: true, alarms: []))
+        groups.append(Group(name: "Disabled group", playSound: true, enabled: false, alarms: []))
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groups.count
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        groups.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let group = groups[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Group", for: indexPath)
+        cell.textLabel?.text = group.name
+        
+        if group.enabled {
+            cell.textLabel?.textColor = UIColor.black
+        } else {
+            cell.textLabel?.textColor = UIColor.red
+        }
+        
+        if group.alarms.count == 1 {
+            cell.detailTextLabel?.text = "1 alarm"
+        } else {
+            cell.detailTextLabel?.text = "\(group.alarms.count) alarms"
+        }
+        
+        return cell
     }
 }
 
