b6ff3a1ec2275c23d8742d49cf426a6ecfe5f566
Yury Shkoda <shkoda.yuri@gmail.com>
2018-10-19 11:28:15 -0400

    Added User Interface

---
+++ Alarminator/AlarmViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AlarmViewController.swift
+//  Alarminator
+//
+//  Created by Yury on 10/19/18.
+//  Copyright © 2018 Yury Shkoda. All rights reserved.
+//
+
+import UIKit
+
+class AlarmViewController: UITableViewController {
+
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var caption: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var tapToSelectImage: UILabel!
+    
+    @IBAction func datePickerChanged(_ sender: Any) {
+    }
+    
+    @IBAction func imageViewTapped(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Alarminator/GroupViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GroupViewController.swift
+//  Alarminator
+//
+//  Created by Yury on 10/19/18.
+//  Copyright © 2018 Yury Shkoda. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UITableViewController {
+
+    @IBAction func swichChanged(_ sender: UISwitch) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
